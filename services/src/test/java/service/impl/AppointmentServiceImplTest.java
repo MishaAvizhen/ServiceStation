9a741f69edfc92bee9142cd37ee852009b03ff95
MishaAvizhen <misha33379@mail.com>
2021-03-24 10:52:58 +0300

    add test for appointment

---
+++ services/src/test/java/service/impl/AppointmentServiceImplTest.java
@@ -0,0 +1,41 @@
+package service.impl;
+
+import common.AppointmentTestData;
+import entity.Appointment;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import repository.AppointmentRepository;
+import repository.UserRepository;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AppointmentServiceImplTest {
+    private AppointmentTestData appointmentTestData = AppointmentTestData.getInstance();
+    @Mock
+    private AppointmentRepository appointmentRepository;
+    @Mock
+    private UserRepository userRepository;
+    @InjectMocks
+    private AppointmentServiceImpl appointmentService;
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void findAllAppointment() throws Exception {
+        when(appointmentRepository.findAll()).thenReturn(appointmentTestData.getAllAppointments());
+        List<Appointment> actualAppointments = appointmentService.findAllAppointment();
+        Assert.assertEquals(appointmentTestData.getAllAppointments().size(), actualAppointments.size());
+    }
+
+}
